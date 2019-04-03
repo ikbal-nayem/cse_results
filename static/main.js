a34b60f041e1db039381dba3975c4c5d0aeed137
root <ikbalnayem000@gmail.com>
2019-04-04 03:47:58 +0600

    nothing

--- static/main.js
+++ static/main.js
@@ -2,3 +2,12 @@ fileName = function(){
     var n = document.getElementById('inputFile').files[0].name
     document.getElementById('viewFileName').innerHTML = n
 }
+fileName = function(){
+    var n = document.getElementById('inputFile').files[0].name
+    document.getElementById('viewFileName').innerHTML = n
+}
+progressBar = function(){
+    document.getElementById('progressBar').style.visibility = 'visible'
+    document.getElementById('btnHelp').style.visibility = 'hidden'
+    document.getElementById('btn').classList.add("disabled")
+}
